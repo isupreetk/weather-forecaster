4165d370aaa5baa7d2fbcc8f1d4877b23137f9f9
Supreet Kaur <supreet97@gmail.com>
2023-11-02 12:30:49 -0700

    unsplash api for city image

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,31 @@
 import './App.scss';
 import axios from "axios";
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 function App() {
 
   let api_key = "d1e9a59db3374dd3a00225001230111";
   let api_URL = "http://api.weatherapi.com/v1";
   // console.log(`${api_URL}/current.json?key=${api_key}&q=Vancouver`);
+  let formRef = useRef();
 
   let [condition, setCondition] = useState();
   let [darkMode, setDarkMode] = useState(false);
   let [theme, setTheme] = useState("light");
   let [icon, setIcon] = useState("🌙");
+  let [cityImage, setCityImage] = useState();
 
   const toggleDarkMode = () => {
-      setDarkMode(!darkMode);
-      if (!darkMode) {
-        setTheme("dark");
-        setIcon("🔆");
-      }
-      else {
-        setTheme("light");
-        setIcon("🌙");
-      }
-      
+    setDarkMode(!darkMode);
+    if (!darkMode) {
+      setTheme("dark");
+      setIcon("🔆");
+    }
+    else {
+      setTheme("light");
+      setIcon("🌙");
+    }
+
   };
 
   function getWeather(event) {
@@ -39,40 +41,52 @@ function App() {
       .catch((error) => {
         console.log(error);
       })
+
+    axios.get(`https://api.unsplash.com/search/photos/?client_id=8FZ6HGQVeeTdXK7PzSe-oG5fvseGAqntYJxsQJvvmBk&query=${event.target.city.value}`)
+      .then((response) => {
+        // console.log(response.data.results);
+        // console.log(response.data.results[0].links?.download);
+        setCityImage(response.data.results[0].links?.download);
+      })
+
+      formRef.current.reset();
   }
 
   return (
 
     <div className={`App ${theme}`}>
 
-      <button onClick={toggleDarkMode} className='toggle-btn'>{icon}</button>
+        <button onClick={toggleDarkMode} className='toggle-btn'>{icon}</button>
 
-      <header className="App-header">
-        <h1 className='App-header__title'>Check the current weather now!</h1>
-      </header>
-      <form className='form' onSubmit={(event) => getWeather(event)}>
-        <label htmlFor='city'>City</label>
-        <input name="city" className='form__input'></input>
-        <label htmlFor='country'>Country</label>
-        <input name="country" className='form__input'></input>
-        <button className='form__button'>Check</button>
-      </form>
+        <header className="App-header">
+          <h1 className='App-header__title'>Check the current weather now!</h1>
+        </header>
+        <form className='form' ref={formRef} onSubmit={(event) => getWeather(event)}>
+          <input name="city" className='form__input' placeholder="Enter the city"></input>
+          <button className='form__button'>Check</button>
+        </form>
 
-    { condition ? 
-      (
-      <div className='output'>
-        <p>Text: {condition.current?.condition.text}</p>
-        <p>Icon: <img src={condition.current?.condition.icon} /></p>
-        <p>Temperature: {condition.current?.temp_c} Celsius</p>
-        <p>Feels Like: {condition.current?.feelslike_c} Celsius</p>
-        <p>Humidity: {condition.current?.humidity}</p>
-        <p>Wind Speed: {condition.current?.wind_kph} kph</p>
-        <p>Precipitation: {condition.current?.precip_in} in</p>
-      </div>
-      )
-      : 
-      <div></div>
-}  
+        {condition ?
+          (
+            <div className='output'>
+              <div className='output__container1'>
+                <h3>{condition.location.name}</h3>
+              </div>
+              <div className='output__container2'></div>
+              
+              <img src={cityImage} className='city' alt="city" />
+              <p>Text: {condition.current?.condition.text}</p>
+              <p>Icon: <img src={condition.current?.condition.icon} alt="weather status"/></p>
+              <p>Temperature: {condition.current?.temp_c}°C</p>
+              <p>Feels Like: {condition.current?.feelslike_c}°C</p>
+              <p>Humidity: {condition.current?.humidity}%</p>
+              <p>Wind Speed: {condition.current?.wind_kph} km/h</p>
+              <p>Precipitation: {condition.current?.precip_in} in</p>
+            </div>
+          )
+          :
+          <div></div>
+        } 
     </div>
 
   );
